2c2194cd2f394f813acc0f80df440d27d621bd5a
Bramwel <bramwelngayo@gmail.com>
2021-04-02 18:16:30 +0300

    Created clincs index, create and edit pages

---
+++ resources/js/Pages/Clinics/Create.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { Inertia } from '@inertiajs/inertia';
+import { InertiaLink, useForm } from '@inertiajs/inertia-react';
+import Layout from '@/Shared/Layout';
+import LoadingButton from '@/Shared/LoadingButton';
+import TextInput from '@/Shared/TextInput';
+import SelectInput from '@/Shared/SelectInput';
+
+const Create = () => {
+  const { data, setData, errors, post, processing } = useForm({
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+    location: '',
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    post(route('clinics.store'));
+  }
+
+  return (
+    <div>
+      <h1 className="mb-8 text-3xl font-bold">
+        <InertiaLink
+          href={route('clinics')}
+          className="text-indigo-600 hover:text-indigo-700"
+        >
+          Clinics
+        </InertiaLink>
+        <span className="font-medium text-indigo-600"> /</span> Add Clinic
+      </h1>
+      <div className="max-w-3xl overflow-hidden bg-white rounded shadow">
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-wrap p-8 -mb-8 -mr-6">
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Name"
+              name="name"
+              errors={errors.name}
+              value={data.name}
+              onChange={e => setData('name', e.target.value)}
+            />
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Email"
+              name="email"
+              type="email"
+              errors={errors.email}
+              value={data.email}
+              onChange={e => setData('email', e.target.value)}
+            />
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Phone"
+              name="phone"
+              type="text"
+              errors={errors.phone}
+              value={data.phone}
+              onChange={e => setData('phone', e.target.value)}
+            />
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Address"
+              name="address"
+              type="text"
+              errors={errors.address}
+              value={data.address}
+              onChange={e => setData('address', e.target.value)}
+            />
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Location"
+              name="location"
+              type="text"
+              errors={errors.location}
+              value={data.location}
+              onChange={e => setData('location', e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-end px-8 py-4 bg-gray-100 border-t border-gray-200">
+            <LoadingButton
+              loading={processing}
+              type="submit"
+              className="btn-indigo"
+            >
+              Add Clinic
+            </LoadingButton>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+Create.layout = page => <Layout title="Add Clinic" children={page} />;
+
+export default Create;
---
+++ resources/js/Pages/Clinics/Edit.js
@@ -0,0 +1,206 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { Inertia } from '@inertiajs/inertia';
+import { InertiaLink, usePage, useForm } from '@inertiajs/inertia-react';
+import Layout from '@/Shared/Layout';
+import DeleteButton from '@/Shared/DeleteButton';
+import LoadingButton from '@/Shared/LoadingButton';
+import TextInput from '@/Shared/TextInput';
+import SelectInput from '@/Shared/SelectInput';
+import TrashedMessage from '@/Shared/TrashedMessage';
+import Icon from '@/Shared/Icon';
+
+const Edit = () => {
+  const { clinic } = usePage().props;
+  const { data, setData, errors, put, processing } = useForm({
+    name: clinic.name || '',
+    email: clinic.email || '',
+    phone: clinic.phone || '',
+    address: clinic.address || '',
+    location: clinic.location || '',
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    put(route('clinics.update', clinic.id));
+  }
+
+  function destroy() {
+    if (confirm('Are you sure you want to delete this clinic?')) {
+      Inertia.delete(route('clinics.destroy', clinic.id));
+    }
+  }
+
+  function restore() {
+    if (confirm('Are you sure you want to restore this clinic?')) {
+      Inertia.put(route('clinics.restore', clinic.id));
+    }
+  }
+
+  return (
+    <div>
+      <Helmet title={data.name} />
+      <h1 className="mb-8 text-3xl font-bold">
+        <InertiaLink
+          href={route('organizations')}
+          className="text-indigo-600 hover:text-indigo-700"
+        >
+          Organizations
+        </InertiaLink>
+        <span className="mx-2 font-medium text-indigo-600">/</span>
+        {data.name}
+      </h1>
+      {organization.deleted_at && (
+        <TrashedMessage onRestore={restore}>
+          This organization has been deleted.
+        </TrashedMessage>
+      )}
+      <div className="max-w-3xl overflow-hidden bg-white rounded shadow">
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-wrap p-8 -mb-8 -mr-6">
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Name"
+              name="name"
+              errors={errors.name}
+              value={data.name}
+              onChange={e => setData('name', e.target.value)}
+            />
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Email"
+              name="email"
+              type="email"
+              errors={errors.email}
+              value={data.email}
+              onChange={e => setData('email', e.target.value)}
+            />
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Phone"
+              name="phone"
+              type="text"
+              errors={errors.phone}
+              value={data.phone}
+              onChange={e => setData('phone', e.target.value)}
+            />
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Address"
+              name="address"
+              type="text"
+              errors={errors.address}
+              value={data.address}
+              onChange={e => setData('address', e.target.value)}
+            />
+            <TextInput
+              className="w-full pb-8 pr-6 lg:w-1/2"
+              label="Location"
+              name="location"
+              type="text"
+              errors={errors.location}
+              value={data.location}
+              onChange={e => setData('location', e.target.value)}
+            />
+          </div>
+          <div className="flex items-center px-8 py-4 bg-gray-100 border-t border-gray-200">
+            {!clinic.deleted_at && (
+              <DeleteButton onDelete={destroy}>
+                Delete Clinic
+              </DeleteButton>
+            )}
+            <LoadingButton
+              loading={processing}
+              type="submit"
+              className="ml-auto btn-indigo"
+            >
+              Update Clinic
+            </LoadingButton>
+          </div>
+        </form>
+      </div>
+      <h2 className="mt-12 text-2xl font-bold">Contacts</h2>
+      <div className="mt-6 overflow-x-auto bg-white rounded shadow">
+        <table className="w-full whitespace-nowrap">
+          <thead>
+            <tr className="font-bold text-left">
+              <th className="px-6 pt-5 pb-4">Name</th>
+              <th className="px-6 pt-5 pb-4">City</th>
+              <th className="px-6 pt-5 pb-4" colSpan="2">
+                Phone
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {organization.contacts.map(
+              ({ id, name, phone, city, deleted_at }) => {
+                return (
+                  <tr
+                    key={id}
+                    className="hover:bg-gray-100 focus-within:bg-gray-100"
+                  >
+                    <td className="border-t">
+                      <InertiaLink
+                        href={route('contacts.edit', id)}
+                        className="flex items-center px-6 py-4 focus:text-indigo focus:outline-none"
+                      >
+                        {name}
+                        {deleted_at && (
+                          <Icon
+                            name="trash"
+                            className="flex-shrink-0 w-3 h-3 ml-2 text-gray-400 fill-current"
+                          />
+                        )}
+                      </InertiaLink>
+                    </td>
+                    <td className="border-t">
+                      <InertiaLink
+                        tabIndex="-1"
+                        href={route('contacts.edit', id)}
+                        className="flex items-center px-6 py-4 focus:text-indigo focus:outline-none"
+                      >
+                        {city}
+                      </InertiaLink>
+                    </td>
+                    <td className="border-t">
+                      <InertiaLink
+                        tabIndex="-1"
+                        href={route('contacts.edit', id)}
+                        className="flex items-center px-6 py-4 focus:text-indigo focus:outline-none"
+                      >
+                        {phone}
+                      </InertiaLink>
+                    </td>
+                    <td className="w-px border-t">
+                      <InertiaLink
+                        tabIndex="-1"
+                        href={route('contacts.edit', id)}
+                        className="flex items-center px-4"
+                      >
+                        <Icon
+                          name="cheveron-right"
+                          className="block w-6 h-6 text-gray-400 fill-current"
+                        />
+                      </InertiaLink>
+                    </td>
+                  </tr>
+                );
+              }
+            )}
+            {organization.contacts.length === 0 && (
+              <tr>
+                <td className="px-6 py-4 border-t" colSpan="4">
+                  No contacts found.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+Edit.layout = page => <Layout children={page} />;
+
+export default Edit;
