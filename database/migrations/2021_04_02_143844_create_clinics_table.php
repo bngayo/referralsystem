8759993757bd533859f7ef679c5cf1f62e48e978
Bramwel <bramwelngayo@gmail.com>
2021-04-02 18:00:40 +0300

    Added Clinic and Patient models, factories and controllers

---
+++ database/migrations/2021_04_02_143844_create_clinics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClinicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clinics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('email', 50)->nullable();
+            $table->string('phone', 50)->nullable();
+            $table->string('address', 150)->nullable();
+            $table->string('location', 50)->nullable();
+            $table->string('county', 50)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clinics');
+    }
+}
---
+++ database/migrations/2021_04_02_144917_create_patients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('clinic_id')->index();
+            $table->string('first_name', 25);
+            $table->string('last_name', 25);
+            $table->string('email', 50)->nullable();
+            $table->string('phone', 50)->nullable();
+            $table->string('nhif_number', 25)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
