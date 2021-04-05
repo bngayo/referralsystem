3ea065798be655df083cd21b022cd8352430c8cf
Bramwel <bramwelngayo@gmail.com>
2021-04-05 11:51:04 +0300

    Added St Jude logo

--- resources/js/Pages/Auth/Login.js
+++ resources/js/Pages/Auth/Login.js
@@ -22,16 +22,18 @@ export default () => {
     <div className="flex items-center justify-center min-h-screen p-6 bg-indigo-900">
       <Helmet title="Login" />
       <div className="w-full max-w-md">
-        <Logo
-          className="block w-full max-w-xs mx-auto text-white fill-current"
-          height={50}
-        />
+
+       
         <form
           onSubmit={handleSubmit}
-          className="mt-8 overflow-hidden bg-white rounded-lg shadow-xl"
+          className="mt-4 overflow-hidden bg-white rounded-lg shadow-xl"
         >
-          <div className="px-10 py-12">
-            <h1 className="text-3xl font-bold text-center">Welcome Back!</h1>
+          <Logo
+            className="block w-full max-w-xs mx-auto text-white fill-current"
+          />
+
+          <div className="px-2 py-12">
+            <h1 className="text-3xl font-bold text-center">Login</h1>
             <div className="w-24 mx-auto mt-6 border-b-2" />
             <TextInput
               className="mt-10"
