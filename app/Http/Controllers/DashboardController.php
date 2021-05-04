63e49411b696252345ad6c0cd9dea78ef01ba86b
Bramwel <bramwelngayo@gmail.com>
2021-05-04 15:55:32 +0300

    Added stats from the database

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use App\Models\Clinic;
+use App\Models\Referral;
+use App\Models\Patient;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return Inertia::render('Dashboard/Index');
+        return Inertia::render('Dashboard/Index', [
+            'clinics'=> Clinic::count(),
+            'patients'=> Patient::count(),
+            'referrals'=> Referral::count(),
+        ]);
     }
 }
--- resources/js/Pages/Dashboard/Index.js
+++ resources/js/Pages/Dashboard/Index.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { InertiaLink } from '@inertiajs/inertia-react';
+import { usePage } from '@inertiajs/inertia-react';
 import Layout from '@/Shared/Layout';
-import DashboardStats from '../../Shared/DashboardStats';
+import DashboardStat from '../../Shared/DashboardStat';
 
 const Dashboard = () => {
+
+  const { clinics, patients, referrals } = usePage().props;
+  
   return (
     <div>
       <h1 className="mb-8 text-3xl font-bold">Dashboard</h1>
 
-      <DashboardStats/>
-
-      {/* <div>
-        <InertiaLink className="mr-1 btn-indigo" href="/500">
-          500 error
-        </InertiaLink>
-        <InertiaLink className="btn-indigo" href="/404">
-          404 error
-        </InertiaLink>
-      </div> */}
+      <div id="wrapper" class="max-w-xl px-4 py-4 mx-auto">
+            <div class="grid sm:h-32 sm:grid-flow-row sm:gap-4 sm:grid-cols-3">
+                <DashboardStat title = "Clinics" number={clinics} percentage="6%" textColor="text-green-500" iconName="positive"/>
+                <DashboardStat title = "Patients" number={patients} percentage="0%" textColor="text-gray-500" iconName="neutral"/>
+                <DashboardStat title = "Referrals" number={referrals} percentage="2%" textColor="text-red-500" iconName="negative"/>
+            </div>
+        </div>
     </div>
   );
 };
